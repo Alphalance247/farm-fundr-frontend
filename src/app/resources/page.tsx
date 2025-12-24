1a22baa44238d86d8562aa294a3b5e457761f7c4
Azeez <al.moshood017@gmail.com>
2025-12-24 09:28:57 +0100

    refactor(resources): remove unused imports from Resources page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,5 +1,3 @@
-import HeroCommon from "../components/common/heroCommon";
-import GetInTouch from "../components/ContactUs/GetInTouch";
 import LayOuts from "../components/common/Layouts";
 import Container from "../components/common/container";
 
